a48c1709a4a5707222641dc7ef29da55a68ef53f
Quentin de Quelen <gitlab@dequelen.eu>
2016-01-23 12:13:00 +0100

    create files for show map

---
+++ app/scripts/controllers/map.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xavierContactApp.controller:MapCtrl
+ * @description
+ * # MapCtrl
+ * Controller of the xavierContactApp
+ */
+angular.module('xavierContactApp')
+  .controller('MapCtrl', function ($scope) {
+
+  });
