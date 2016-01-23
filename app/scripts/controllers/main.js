fb757bf6a024f49155e59e64ab977fed1c3ebeeb
Quentin de Quelen <gitlab@dequelen.eu>
2016-01-23 11:19:43 +0100

    create yeoman project with angular bootstrap

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xavierContactApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xavierContactApp
+ */
+angular.module('xavierContactApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
