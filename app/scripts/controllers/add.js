15697b32b89ec2a20128b58a932dad9428f1bdc4
Baptiste Jamin <baptistejamin@gmail.com>
2016-01-23 13:56:07 +0100

    Add contact with geolocation

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -8,12 +8,21 @@
  * Controller of the xavierContactApp
  */
 angular.module('xavierContactApp')
-  .controller('AddCtrl', function ($scope, Contacts) {
+  .controller('AddCtrl', function ($scope, Contacts, Geocoder) {
     $scope.contact = {};
 
     $scope.addContact = function(){
-      Contacts.addContact($scope.contact).then(function(contact) {
-        alert("Your contact is added");
-      });
+      Geocoder.getLocation(
+        $scope.contact.number,
+        $scope.contact.street,
+        $scope.contact.city,
+        $scope.contact.postcode
+        )
+        .then(function(position) {
+          $scope.contact.position = position;
+          Contacts.addContact($scope.contact).then(function(){
+            alert("contact added!");
+          });
+        });
     };
   });
