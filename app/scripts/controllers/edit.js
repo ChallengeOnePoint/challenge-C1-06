44e58ce56fe91b53ec720dd0b2a1ada25ebb73e6
Baptiste Jamin <baptistejamin@gmail.com>
2016-01-23 12:25:53 +0100

    Edit contact

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -8,6 +8,15 @@
  * Controller of the xavierContactApp
  */
 angular.module('xavierContactApp')
-  .controller('EditCtrl', function ($scope) {
+  .controller('EditCtrl', function ($scope, Contacts, $routeParams) {
+    $scope.contact = {};
+    Contacts.getContact($routeParams.id).$loaded().then(function(contact) {
+      $scope.contact = contact;
+    });
 
+    $scope.save = function(){
+      $scope.contact.$save().then(function(){
+        alert("Contact saved :)");
+      })
+    };
   });
